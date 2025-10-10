74a165808aed3578c13f1515de8d9b175253b56b
alouche-bd <a.louche@biotech-dental.com>
2025-10-10 19:26:36 +0200

    deploy

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import {NextRequest} from 'next/server';
 import createMiddleware from 'next-intl/middleware';
-import {routing} from '@/lib/i18n/routing';
+import {routing} from './lib/i18n/routing';
 
 const intlMiddleware = createMiddleware(routing);
 
